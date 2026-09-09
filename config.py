6fdad71959e03ed05a3bed822ffd42658bd71597
Ilia193 <147809013+Ilia193@users.noreply.github.com>
2026-09-09 16:40:19 +0300

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 # Токен бота — получить у @id199142634 (@BotFather) в Telegram
-BOT_TOKEN = os.getenv("8730107914:AAFltLnXRFlMN5qwiTMuFRRu0EzRLAomZjk")
-ADMIN_IDS = [int(x) for x in os.getenv("839849374").split(",") if x.strip()]
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMINS_IDS", "839849374").split(",") if x.strip()]
 DB_PATH = "vpn_bot.db"
